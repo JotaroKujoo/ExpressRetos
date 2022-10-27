fcc3fb2ddbdb853c7e73c199d520fb8dcf224ba1
AdryDevs <alonsoadrian86@gmail.com>
2022-10-27 19:13:38 +0200

    router page finished

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-// const routes = require('./router');
+const router = require('../ExpressRetos/router');
 const PORT = 3000;
 
 app.use(express.json());
-// app.use(router)
+app.use(router)
 
 app.listen(PORT, () => {
     console.log(`Server is ON!! using port ${PORT}`);
